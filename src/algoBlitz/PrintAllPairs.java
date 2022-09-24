6163780428afcac57a2f6a06c465c575448d330c
Jared Bears <jaredbears@outlook.com>
2022-09-24 15:29:12 -0500

    Print All Pairs

---
+++ src/algoBlitz/PrintAllPairs.java
@@ -0,0 +1,24 @@
+package algoBlitz;
+
+public class PrintAllPairs {
+  public static void main(String[] args) {
+    
+    int[] testArrayOne = { };
+    int[] testArrayTwo = {1, 2, 3};
+    int[] testArrayThree = {1, 2, 3, 4};
+    int[] testArrayFour = {1, 1};
+    
+    printAllPairs(testArrayOne);
+    printAllPairs(testArrayTwo);
+    printAllPairs(testArrayThree);
+    printAllPairs(testArrayFour);
+  }
+  
+  static void printAllPairs(int[] arr) {
+    for(int i = 0; i < arr.length; i++) {
+      for(int j = i + 1; j < arr.length; j++) {
+        System.out.println(arr[i] + ", " + arr[j]);
+      }
+    }
+  }
+}
