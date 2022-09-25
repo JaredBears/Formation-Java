d2b3ba011ef68094011cbc3880a5204e68983559
Jared Bears <jaredbears@outlook.com>
2022-09-24 23:00:15 -0500

    -

---
+++ src/algoBlitz/arrays/MatchFellows.java
@@ -0,0 +1,48 @@
+package algoBlitz.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MatchFellows {
+
+  public static void main(String[] args) {
+    HashMap<String, Integer> fellows = new HashMap<>();
+    fellows.put("Oliver", 3);
+    fellows.put("Pixel", 3);
+    fellows.put("Pinky", 5);
+    fellows.put("Tobey", 5);
+    
+    HashMap<String, Integer> fellowsTwo = new HashMap<>();
+    fellowsTwo.put("Oliver", 3);
+    fellowsTwo.put("Pixel", 4);
+    fellowsTwo.put("Pinky", 5);
+    fellowsTwo.put("Tobey", 5);
+    
+    HashMap<String, Integer> fellowsThree = new HashMap<>();
+    
+    HashMap<String, Integer> fellowsFour = new HashMap<>();
+    fellowsFour.put("Oliver", 3);
+    fellowsFour.put("Pixel", 3);
+    fellowsFour.put("Pinky", 5);
+    
+    System.out.println(canMatchFellows(fellows));
+    System.out.println(canMatchFellows(fellowsTwo));
+    System.out.println(canMatchFellows(fellowsThree));
+    System.out.println(canMatchFellows(fellowsFour));
+
+  }
+  
+  static boolean canMatchFellows(HashMap<String, Integer> fellows) {
+    HashSet<Integer> set = new HashSet<>();
+    
+    fellows.forEach((key, value) -> {
+      if(set.contains(value)) {
+        set.remove(value);
+      } else {
+        set.add(value);
+      }
+    });
+    return set.isEmpty();
+  }
+
+}
---
+++ src/algoBlitz/arrays/SumInReverse.java
@@ -0,0 +1,27 @@
+package algoBlitz.arrays;
+
+import java.util.Arrays;
+
+public class SumInReverse {
+  
+  public static void main(String[] args) {
+    int[] testArrayOne = {1, 2, 3};
+    int[] testArrayTwo = {4, 6, 10};
+    int[] testArrayThree = {1, 5, 10, 12};
+    int[] testArrayFour = {2, 4, 3, 5};
+    
+    System.out.println(Arrays.toString(sumInReverse(testArrayOne, testArrayTwo)));
+    System.out.println(Arrays.toString(sumInReverse(testArrayThree, testArrayFour)));
+  }
+  
+  static int[] sumInReverse(int[] arrOne, int[] arrTwo) {
+    int[] result = new int[arrOne.length];
+    
+    for(int i = 0; i < arrOne.length; i ++) {
+      result[i] = arrOne[i] + arrTwo[(arrTwo.length - 1) - i];
+    }
+    
+    return result;
+  }
+
+}
